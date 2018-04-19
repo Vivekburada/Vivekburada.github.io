404f3825683a36cb670e631fb2416b81fd75eb80
vivekburada <vivekburada@github.com>
2018-04-19 09:29:00 +0530

    added  title clear

--- d_title.php
+++
@@ -1,9 +0,0 @@
-
-<div class='tiles' >
-    <a href="button1.php" ><input type="button" class="buttonstyler" value="TASKS"></a>
-    <a href="button2.php" >  <input type="button" class="buttonstyler" value=" Button 1"></a>
-      <a href="button3.php" ><input type="button" id="buttonstyler" value="Button 2"></a>
-  <a href="logout.php" ><input type="button" class="buttonstyler" value="Logout"></a>
-
-
-</div>
